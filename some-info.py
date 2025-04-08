bafba1828ac97a99fdec739c9cf04ae4cf2c802a
Vaibhav Priyadarshi <115949400+vaibhavgeometer@users.noreply.github.com>
2025-04-08 22:01:01 +0530

    Update some-info.py

--- some-info.py
+++ some-info.py
@@ -2,5 +2,5 @@
 For 1st Sem
 Name, Roll Number, Date of Birth(YYYY-MM-DD), SGPA
 Vaibhav Priyadarshi, 2515075160387, 2003-06-12
-Aarohi Gond, 2515075160274, ???
+Aarohi Gond, 2515075160274, 2007-01-01
 '''
